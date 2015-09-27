e5282a6f88dc50b52fb3b05ebe3b6b29816db433
liuxueyang <zypz457@sina.com>
2015-09-27 18:24:26 +0800

    change font to Inconsolata

--- init.el
+++ init.el
@@ -80,7 +80,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#2d2d2d" :foreground "#cccccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "unknown" :family "Monospace"))))
+ '(default ((t (:inherit nil :stipple nil :background "#2d2d2d" :foreground "#cccccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Inconsolata"))))
  '(highlight-stages-level-1-face ((t (:background "OliveDrab1"))))
  '(highlight-stages-level-2-face ((t (:background "honeydew1"))))
  '(highlight-stages-level-3-face ((t (:background "LightPink4"))))
