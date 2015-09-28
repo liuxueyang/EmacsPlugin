04547cca260d84e51eac57fbb50dfe292a45c916
liuxueyang <zypz457@sina.com>
2015-09-28 19:40:36 +0800

    add dired+ plugin

--- init.el
+++ init.el
@@ -80,7 +80,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#2d2d2d" :foreground "#cccccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Inconsolata"))))
+ '(default ((t (:inherit nil :stipple nil :background "#2d2d2d" :foreground "#cccccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Inconsolata"))))
  '(highlight-stages-level-1-face ((t (:background "OliveDrab1"))))
  '(highlight-stages-level-2-face ((t (:background "honeydew1"))))
  '(highlight-stages-level-3-face ((t (:background "LightPink4"))))
@@ -170,10 +170,14 @@
 (icy-mode 1)
 
 ;; --------------------
-;; slime
+;; ace-window
+(global-set-key (kbd "M-p") 'ace-window)
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+(setq aw-dispatch-always t)
 
 ;; --------------------
-;; slime
+;;
+
 
 ;; --------------------
 ;; macrostep
