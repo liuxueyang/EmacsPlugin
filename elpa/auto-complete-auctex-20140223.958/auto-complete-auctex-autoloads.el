61cb5e18ed23ed605368319e550476c11e2cbca6
liuxueyang <zypz457@sina.com>
2015-11-23 19:56:57 +0800

    add latex plugin

---
+++ elpa/auto-complete-auctex-20140223.958/auto-complete-auctex-autoloads.el
@@ -0,0 +1,16 @@
+;;; auto-complete-auctex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("auto-complete-auctex.el") (22098 58054
+;;;;;;  316959 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; auto-complete-auctex-autoloads.el ends here
