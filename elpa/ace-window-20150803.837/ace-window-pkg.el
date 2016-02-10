37a99cfcef8b9016aa4d9f35d7ce917a24e4d570
liuxueyang <zypz457@sina.com>
2016-02-10 19:53:30 +0800

    change git ignore

---
+++ elpa/ace-window-20150803.837/ace-window-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ace-window" "20150803.837" "Quickly switch windows." (quote ((avy "0.2.0"))))
---
+++ elpa/avy-20160202.2357/avy-pkg.el
@@ -0,0 +1,1 @@
+(define-package "avy" "20160202.2357" "tree-based completion" (quote ((emacs "24.1") (cl-lib "0.5"))))
