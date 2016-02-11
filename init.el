c87cd3ec75d6d03b697c6cb9683e768b20d435f6
liuxueyang <zypz457@sina.com>
2016-02-11 13:29:50 +0800

    change rainbow-blocks color

--- init.el
+++ init.el
@@ -22,9 +22,9 @@
     highlight-sexp))
 
 (mapcar #'(lambda (package)
-        (unless (package-installed-p package)
-          (package-install package)))
-    myPackages)
+            (unless (package-installed-p package)
+              (package-install package)))
+        myPackages)
 
 (setq inhibit-startup-message t)
 
@@ -41,6 +41,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ ;; '(rainbow-blocks-depth-1-face ((t (:foreground "SpringGreen4"))))
+ ;; '(rainbow-blocks-depth-2-face ((t (:foreground "dark cyan"))))
+ ;; '(rainbow-blocks-depth-3-face ((t (:foreground "deep sky blue"))))
+ ;; '(rainbow-blocks-depth-4-face ((t (:foreground "slate blue"))))
+ ;; '(rainbow-blocks-depth-5-face ((t (:foreground "medium orchid"))))
+ ;; '(rainbow-blocks-depth-6-face ((t (:foreground "indian red"))))
+ ;; '(rainbow-blocks-depth-7-face ((t (:foreground "peach puff"))))
+ ;; '(rainbow-blocks-depth-8-face ((t (:foreground "light green"))))
+ ;; '(rainbow-blocks-depth-9-face ((t (:foreground "forest green"))))
+
  '(rainbow-blocks-depth-1-face ((t (:foreground "SpringGreen4"))))
  '(rainbow-blocks-depth-2-face ((t (:foreground "dark cyan"))))
  '(rainbow-blocks-depth-3-face ((t (:foreground "deep sky blue"))))
@@ -49,7 +59,19 @@
  '(rainbow-blocks-depth-6-face ((t (:foreground "indian red"))))
  '(rainbow-blocks-depth-7-face ((t (:foreground "peach puff"))))
  '(rainbow-blocks-depth-8-face ((t (:foreground "light green"))))
- '(rainbow-blocks-depth-9-face ((t (:foreground "forest green")))))
+ '(rainbow-blocks-depth-9-face ((t (:foreground "forest green"))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "dark magenta"))))
+ '(rainbow-delimiters-mismatched-face ((t (:inherit rainbow-delimiters-unmatched-face :background "green"))))
+ '(rainbow-delimiters-unmatched-face ((t (:background "yellow"))))
+ )
 
 (setq default-major-mode 'text-mode)
 
@@ -103,8 +125,37 @@
 (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
-;; solaried-dark theme
+;; solarized-dark theme
 (load-theme 'solarized-dark t)
+
+;; make the fringe stand out from the background
+(setq solarized-distinct-fringe-background t)
+
+;; Don't change the font for some headings and titles
+(setq solarized-use-variable-pitch nil)
+
+;; make the modeline high contrast
+(setq solarized-high-contrast-mode-line t)
+
+;; Use less bolding
+(setq solarized-use-less-bold t)
+
+;; Use more italics
+(setq solarized-use-more-italic t)
+
+;; Use less colors for indicators such as git:gutter, flycheck and similar
+(setq solarized-emphasize-indicators nil)
+
+;; Don't change size of org-mode headlines (but keep other size-changes)
+(setq solarized-scale-org-headlines nil)
+
+;; Avoid all font-size changes
+(setq solarized-height-minus-1 1)
+(setq solarized-height-plus-1 1)
+(setq solarized-height-plus-2 1)
+(setq solarized-height-plus-3 1)
+(setq solarized-height-plus-4 1)
+;; end of solarized-dark theme
 
 ;; highlight sexp mode
 (add-hook 'lisp-mode-hook 'highlight-sexp-mode)
