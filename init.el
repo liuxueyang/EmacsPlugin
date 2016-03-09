5f8e62a99845c1a5b676628629bbe5e0adca7299
liuxueyang <zypz457@sina.com>
2016-03-09 11:29:53 +0800

    add pretty-lambdada and geiser emacs plugin

--- init.el
+++ init.el
@@ -22,7 +22,9 @@
     highlight-sexp
     multiple-cursors
     geiser
-    guile-scheme))
+    ac-geiser
+    guile-scheme
+    pretty-lambdada))
 
 (mapcar #'(lambda (package)
             (unless (package-installed-p package)
@@ -200,3 +202,32 @@
 
 ;; scheme
 (setq scheme-program-name "~/Bin/scheme")
+
+;; geiser
+(setq geiser-active-implementations '(chicken guile))
+
+(defun ome-geiser-setup ()
+  (setq ome-geiser-impl
+        (or (and (executable-find "racket") 'racket)
+            (and (executable-find "guile") 'guile)
+            (and (executable-find "csi") 'chicken)))
+  (setq geiser-default-implementation ome-geiser-impl)
+  (setq geiser-guile-load-init-file-p t)
+  (add-hook 'geiser-mode-hook
+            (lambda ()
+              (setq geiser-impl--implementation ome-geiser-impl))))
+
+(defun ome-ac-geiser-setup ()
+  (add-hook 'geiser-mode-hook 'ac-geiser-setup)
+  (add-hook 'geiser-repl-mode-hook 'ac-geiser-setup)
+  (eval-after-load "auto-complete"
+    '(add-to-list 'ac-modes 'geiser-repl-mode)))
+
+(when (or (executable-find "racket")
+          (executable-find "guile")
+          (executable-find "chicken"))
+  (ome-install 'geiser)
+  (ome-install 'ac-geiser))
+
+;; pretty-lambdada
+(pretty-lambda-for-modes)
