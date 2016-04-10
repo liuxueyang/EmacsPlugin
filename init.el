3b8500dea40687b5e94f4a6d5cdeb20aff724c53
liuxueyang <zypz457@sina.com>
2016-04-10 22:36:27 +0800

    update rainbow colors

--- init.el
+++ init.el
@@ -9,20 +9,18 @@
 (defvar myPackages
   '(
     better-defaults
-    material-theme
     icicles
     lispy
     slime
     rainbow-blocks
     rainbow-delimiters
-    solarized-theme
     highlight-sexp
     multiple-cursors
     geiser
     pretty-lambdada
     exec-path-from-shell
     2048-game
-    molokai-theme
+    color-theme-modern
     ))
 (mapcar #'(lambda (package)
             (unless (package-installed-p package)
@@ -30,30 +28,33 @@
         myPackages)
 (setq inhibit-startup-message t)
 (global-linum-mode t)
-(load-theme 'material t)
+(load-theme 'charcoal-black t t)
+(enable-theme 'charcoal-black)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rainbow-blocks-depth-1-face ((t (:foreground "dark orange"))))
- '(rainbow-blocks-depth-2-face ((t (:foreground "#8b7500"))))
- '(rainbow-blocks-depth-3-face ((t (:foreground "chartreuse"))))
- '(rainbow-blocks-depth-4-face ((t (:foreground "deep sky blue"))))
- '(rainbow-blocks-depth-5-face ((t (:foreground "yellow"))))
+ '(delete-selection-mode nil)
+ '(rainbow-delimiters-max-face-count 5)
+ '(rainbow-blocks-depth-1-face ((t (:foreground "dark cyan"))))
+ '(rainbow-blocks-depth-2-face ((t (:foreground "dark orange"))))
+ '(rainbow-blocks-depth-3-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-blocks-depth-4-face ((t (:foreground "OrangeRed2"))))
+ '(rainbow-blocks-depth-5-face ((t (:foreground "OliveDrab4"))))
  '(rainbow-blocks-depth-6-face ((t (:foreground "orchid"))))
  '(rainbow-blocks-depth-7-face ((t (:foreground "spring green"))))
  '(rainbow-blocks-depth-8-face ((t (:foreground "sienna"))))
- '(rainbow-blocks-depth-9-face ((t (:foreground "dark magenta"))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "#8b7500"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
+ '(rainbow-blocks-depth-9-face ((t (:foreground "cyan"))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark cyan"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "OrangeRed2"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "OliveDrab4"))))
  '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
  '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
  '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna"))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "dark magenta"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "cyan"))))
  '(rainbow-delimiters-mismatched-face ((t (:inherit rainbow-delimiters-unmatched-face :background "green"))))
  '(rainbow-delimiters-unmatched-face ((t (:background "yellow")))))
 (setq default-major-mode 'text-mode)
@@ -79,16 +80,17 @@
 (setq display-time-day-and-date t)
 (set-face-attribute 'default (selected-frame) :height 140)
 (add-hook 'text-mode-hook 'text-mode-hook-identify)
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
 
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
 ;; --------------------
 ;; transparent
 (set-frame-parameter (selected-frame) 'alpha '(85 85))
-(add-to-list 'default-frame-alist '(alpha 85 85))
 
+(add-to-list 'default-frame-alist '(alpha 85 85))
 ;; lispy-mode
 (add-hook 'lisp-mode-hook (lambda () (lispy-mode 1)))
 (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
+
 (add-hook 'scheme-mode-hook (lambda () (lispy-mode 1)))
 
 (defun conditionally-enable-lispy ()
@@ -96,30 +98,29 @@
     (lispy-mode 1)
     (local-set-key "β" 'helm-lisp-completion-at-point)))
 
-(add-hook 'minibuffer-setup-hook 'conditionally-enable-lispy)
-
- ;; Set your lisp system and, optionally, some contribs
+ (add-hook 'minibuffer-setup-hook 'conditionally-enable-lispy)
+;; Set your lisp system and, optionally, some contribs
 (setq inferior-lisp-program "~/Bin/sbcl")
-(setq slime-contribs '(slime-fancy))
 
+(setq slime-contribs '(slime-fancy))
 ;; rainbow-blocks-mode
 (add-hook 'lisp-mode-hook 'rainbow-blocks-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-blocks-mode)
-(add-hook 'scheme-mode-hook 'rainbow-blocks-mode)
 
+(add-hook 'scheme-mode-hook 'rainbow-blocks-mode)
 ;; rainbow-delimiters-mode
 (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'scheme-mode-hook 'rainbow-delimiters-mode)
 
+(add-hook 'scheme-mode-hook 'rainbow-delimiters-mode)
 ;; highlight sexp mode
 (add-hook 'lisp-mode-hook 'highlight-sexp-mode)
 (add-hook 'emacs-lisp-mode-hook 'highlight-sexp-mode)
+
 (add-hook 'scheme-mode-hook 'highlight-sexp-mode)
-
 (put 'downcase-region 'disabled nil)
-;; I usually use this command to read the expanded macro.
 
+;; I usually use this command to read the expanded macro.
 (cond
  ((eq system-type 'gnu/linux)
   (set-face-attribute 'default nil :font "Hermit-10")
@@ -130,32 +131,35 @@
  (t
   (set-face-attribute 'default nil :font "Monospace-20")
   (set-frame-font "Monospace-20" nil t)))
+
 ;; change emacs font according to the Operating System ;)
-
 ;; scheme
-(setq scheme-program-name "~/Bin/scheme")
 
+(setq scheme-program-name "~/Bin/scheme")
 ;; geiser
+
 (setq geiser-active-implementations '(chicken guile))
 
 ;; pretty-lambdada
 
 (pretty-lambda-for-modes)
-
 ;; exec-path-from-shell
+
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
-
 ;; quicklisp
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-  ;; Replace "sbcl" with the path to your implementation
-(setq inferior-lisp-program "sbcl")
+  (load (expand-file-name "~/quicklisp/slime-helper.el"))
+;; Replace "sbcl" with the path to your implementation
 
+(setq inferior-lisp-program "sbcl")
 ;; frame size
 (setq initial-frame-alist
       '((top . 10)
         (left . 30)
         (width . 90)
         (height . 30)))
+
 (setq default-frame-alist
       '((width . 80) (height . 30)))
+;; autocomplete
+(ac-config-default)
