9dbd278b439dfb58fbb56afaa77efeea4860603c
liuxueyang <zypz457@sina.com>
2016-04-13 15:36:38 +0000

    change to solarized-theme

--- init.el
+++ init.el
@@ -23,6 +23,7 @@
     color-theme-modern
     auto-complete
     multi-term
+    solarized-theme
     ))
 (mapcar #'(lambda (package)
             (unless (package-installed-p package)
@@ -114,9 +115,46 @@
 (add-hook 'scheme-mode-hook 'rainbow-blocks-mode)
 ;; rainbow-delimiters-mode
 (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
-
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'scheme-mode-hook 'rainbow-delimiters-mode)
+
+;; solarized-dark theme
+;; ==============================
+
+;; make the fringe stand out from the background
+(setq solarized-distinct-fringe-background t)
+
+;; Don't change the font for some headings and titles
+(setq solarized-use-variable-pitch nil)
+
+;; make the modeline high contrast
+(setq solarized-high-contrast-mode-line t)
+
+;; Use less bolding
+(setq solarized-use-less-bold t)
+
+;; Use more italics
+(setq solarized-use-more-italic t)
+
+;; Use less colors for indicators such as git:gutter, flycheck and similar
+(setq solarized-emphasize-indicators nil)
+
+;; Don't change size of org-mode headlines (but keep other size-changes)
+(setq solarized-scale-org-headlines nil)
+
+;; Avoid all font-size changes
+(setq solarized-height-minus-1 1)
+(setq solarized-height-plus-1 1)
+(setq solarized-height-plus-2 1)
+(setq solarized-height-plus-3 1)
+(setq solarized-height-plus-4 1)
+
+(load-theme 'solarized-dark t)
+
+;; ==============================
+;; end of solarized-dark theme
+
+
 ;; highlight sexp mode
 (add-hook 'lisp-mode-hook 'highlight-sexp-mode)
 
