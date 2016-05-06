b03bd1d430fe9d31e97d573a5585895912e3e811
liuxueyang <zypz457@sina.com>
2016-05-06 18:11:25 +0800

    add copy-word, copy-line and so on

--- init.el
+++ init.el
@@ -104,7 +104,7 @@
 
 (add-hook 'scheme-mode-hook (lambda () (lispy-mode 1)))
 
- (defun conditionally-enable-lispy ()
+(defun conditionally-enable-lispy ()
   (when (eq this-command 'eval-expression)
     (lispy-mode 1)
     (local-set-key "β" 'helm-lisp-completion-at-point)))
@@ -234,7 +234,7 @@
 ;; exec-path-from-shell
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
-  ;; quicklisp
+;; quicklisp
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 
 ;; Replace "sbcl" with the path to your implementation
@@ -256,7 +256,8 @@
 (add-hook 'term-mode-hook (lambda ()
                             (define-key term-raw-map (kbd "C-y") 'term-paste)))
 ;; turn off visible-bell to avoid opaque on Mac OS X. ;-)
-(setq visible-bell nil)
+;; (setq visible-bell nil)
+(setq ring-bell-function 'ignore)
 
 (setq frame-title-format
       (list
@@ -283,12 +284,12 @@ URL `http://ergoemacs.org/emacs/emacs_copy_file_path.html'
 Version 2015-12-02"
   (interactive "P")
   (let ((fpath (if (equal major-mode 'dired-mode)
-                    (expand-file-name
-                     default-directory)
-                  (if (null (buffer-file-name))
-                      (user-error
-                       "Current buffer is not associated with a file.")
-                    (buffer-file-name)))))
+                   (expand-file-name
+                    default-directory)
+                 (if (null (buffer-file-name))
+                     (user-error
+                      "Current buffer is not associated with a file.")
+                   (buffer-file-name)))))
     (kill-new
      (if (null dir-path-only-p)
          (progn
@@ -303,3 +304,140 @@ Version 2015-12-02"
          (file-name-directory fpath))))))
 
 (global-set-key (kbd "C-c <f5>") 'xah-copy-file-path)
+
+;; https://www.emacswiki.org/emacs/CopyWithoutSelection#toc4
+(defun get-point (symbol &optional arg)
+  "get the point"
+  (funcall symbol arg)
+  (point))
+
+(defun copy-thing (begin-of-thing end-of-thing &optional arg)
+  "copy thing between beg & end into kill ring"
+  (save-excursion
+    (let ((beg (get-point begin-of-thing 1))
+          (end (get-point end-of-thing arg)))
+      (copy-region-as-kill beg end))))
+
+(defun paste-to-mark (&optional arg)
+  "Paste things to mark, or to the prompt in shell-mode"
+  (let ((pasteMe (lambda ()
+                   (if (string=
+                        "shell-mode"
+                        major-mode)
+                       (progn
+                         (comint-next-prompt 25535)
+                         (yank))
+                     (progn
+                       (goto-char (mark))
+                       (yank))))))
+    (if arg
+        (if (= arg 1)
+            nil
+          (funcall pasteMe))
+      (funcall pasteMe))))
+
+(defun copy-word (&optional arg)
+  "Copy words at point into kill-ring"
+  (interactive "P")
+  (copy-thing
+   'backward-word
+   'forward-word
+   arg)
+  ;;(paste-to-mark arg)
+  )
+(global-set-key
+ (kbd "C-c w")
+ (quote copy-word))
+
+(defun copy-line (&optional arg)
+  "Save current line into Kill-Ring without mark the line "
+  (interactive "P")
+  (copy-thing
+   'beginning-of-line
+   'end-of-line
+   arg)
+  (paste-to-mark arg))
+
+(global-set-key
+ (kbd "C-c l")
+ (quote copy-line))
+
+(defun copy-paragraph (&optional arg)
+  "Copy paragraphes at point"
+  (interactive "P")
+  (copy-thing
+   'backward-paragraph
+   'forward-paragraph
+   arg)
+  (paste-to-mark arg))
+
+(global-set-key
+ (kbd "C-c p")
+ (quote copy-paragraph))
+
+(defun beginning-of-string (&optional arg)
+  "  "
+  (re-search-backward
+   "[ \t]"
+   (line-beginning-position)
+   3
+   1)
+  (if (looking-at "[\t ]")
+      (goto-char (+ (point) 1))))
+(defun end-of-string (&optional arg)
+  " "
+  (re-search-forward
+   "[ \t]"
+   (line-end-position)
+   3
+   arg)
+  (if (looking-back "[\t ]")
+      (goto-char (- (point) 1))))
+
+(defun thing-copy-string-to-mark (&optional arg)
+  " Try to copy a string and paste it to the mark
+     When used in shell-mode, it will paste string on shell prompt by default "
+  (interactive "P")
+  (copy-thing
+   'beginning-of-string
+   'end-of-string
+   arg)
+  (paste-to-mark arg))
+
+(global-set-key
+ (kbd "C-c s")
+ (quote
+  thing-copy-string-to-mark))
+
+(defun beginning-of-parenthesis (&optional arg)
+  "  "
+  (re-search-backward
+   "[[<(?\"]"
+   (line-beginning-position)
+   3
+   1)
+  (if (looking-at "[[<(?\"]")
+      (goto-char (+ (point) 1))))
+(defun end-of-parenthesis (&optional arg)
+  " "
+  (re-search-forward
+   "[]>)?\"]"
+   (line-end-position)
+   3
+   arg)
+  (if (looking-back "[]>)?\"]")
+      (goto-char (- (point) 1))))
+
+(defun thing-copy-parenthesis-to-mark (&optional arg)
+  " Try to copy a parenthesis and paste it to the mark
+     When used in shell-mode, it will paste parenthesis on shell prompt by default "
+  (interactive "P")
+  (copy-thing
+   'beginning-of-parenthesis
+   'end-of-parenthesis
+   arg)
+  (paste-to-mark arg))
+(global-set-key
+ (kbd "C-c a")
+ (quote
+  thing-copy-parenthesis-to-mark))
