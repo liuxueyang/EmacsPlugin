7c77f86653ff837322c60caea467122bd771593d
aries <zypz457@sina.com>
2016-05-12 16:42:11 +0800

    change transparency with shortcut

---
+++ SessionDesktop.el
@@ -0,0 +1,44 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Thu May 12 11:39:29 2016
+;; Desktop file format version 206
+;; Emacs version 24.4.51.2
+(setq revive:frame-configuration-to-restore
+  '(progn 
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 218) (left . 785) (unsplittable) (modeline . t) (width . 90) (height . 30) (mouse-color . "black") (environment) (visibility . t) (cursor-color . "#839496") (background-mode . dark) (horizontal-scroll-bars . t) (fullscreen) (alpha 95 95) (scroll-bar-width . 0) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 0) (menu-bar-lines . 0) (right-fringe . 9) (left-fringe . 9) (line-spacing) (background-color . "#002b36") (foreground-color . "#839496") (vertical-scroll-bars) (internal-border-width . 2) (border-width . 0) (font . "-*-Hermit-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(90 30 ((0 0 92 29)) (("~/Library/Preferences/Aquamacs Emacs/Preferences.el" "Preferences.el" 1 1 ((tab "*scratch*" "/Users/aries/Library/Application Support/Aquamacs Emacs/scratch buffer") (tab "Preferences.el" "/Users/aries/Library/Preferences/Aquamacs Emacs/Preferences.el"))))))))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (22323 64241 841966 0) (desktop . "206") "aries@lisp" nil nil ((((font-backend mac-ct ns) (fontsize . 0) (font . "-*-Hermit-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 2) (vertical-scroll-bars) (foreground-color . "#839496") (background-color . "#002b36") (line-spacing) (left-fringe . 9) (right-fringe . 9) (menu-bar-lines . 0) (tool-bar-lines . 0) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 0) (alpha 95 95) (fullscreen) (horizontal-scroll-bars . t) (display-type . color) (background-mode . dark) (cursor-color . "#839496") (visibility . t) (environment) (mouse-color . "black") (frameset--id . "79C3-31FF-1998-0BEA") (frameset--mini t . t) (height . 30) (width . 90) (modeline . t) (minibuffer . t) (unsplittable) (left . 785) (top . 218) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "lisp") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 3) (min-width-ignore . 4) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 92) (min-pixel-width . 90) (min-pixel-height-ignore . 69) (min-pixel-width-ignore . 36) (min-pixel-height-safe . 23) (min-pixel-width-safe . 18)) leaf (pixel-width . 828) (pixel-height . 667) (total-width . 92) (total-height . 29) (normal-height . 1.0) (normal-width . 1.0) (buffer "Preferences.el" (selected . t) (hscroll . 0) (fringes 9 9 nil) (margins 7) (scroll-bars 0 0 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring nil)
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history '("~/Library/Preferences/Aquamacs Emacs/Packages/ido.last" "~/.emacs.d/emms/history" "/Users/aries/Library/Preferences/Aquamacs Emacs/Preferences.el" "/Users/aries/.emacs.d/init.el" "~/Library/Preferences/Aquamacs Emacs/Packages/ido.last" "~/.emacs.d/emms/history" "~/.emacs.d/init.el" "/Users/aries/.emacs.d/init.el" "~/Library/Preferences/Aquamacs Emacs/Packages/ido.last" "~/.emacs.d/emms/history" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/ido-hacks-20150331.1209/ido-hacks-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/solarized-theme-20160505.133/solarized-theme-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/dash-20160510.1127/dash-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/multi-term-20150220.520/multi-term-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/auto-complete-20160416.604/auto-complete-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/popup-20160409.2133/popup-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/color-theme-modern-20160411.1846/color-theme-modern-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/2048-game-20151026.1233/2048-game-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/exec-path-from-shell-20160112.2246/exec-path-from-shell-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/pretty-lambdada-20151231.1548/pretty-lambdada-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/geiser-20160502.1215/geiser-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/multiple-cursors-20160427.1933/multiple-cursors-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/highlight-sexp-1.0/highlight-sexp-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/rainbow-delimiters-20160430.2358/rainbow-delimiters-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/rainbow-blocks-20140306.1033/rainbow-blocks-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/slime-20160419.858/slime-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/macrostep-20151213.145/macrostep-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/lispy-20160511.621/lispy-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/ace-window-20160225.819/ace-window-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/avy-20160503.1901/avy-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/iedit-20150915.2022/iedit-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/swiper-20160510.2311/swiper-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/ivy-20160506.2256/ivy-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/hydra-20160511.152/hydra-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/icicles-20160327.2201/icicles-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/emms-20160304.920/emms-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/nyan-mode-20160429.1323/nyan-mode-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/linum-off-20160217.1337/linum-off-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/volume-20150718.1309/volume-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/bongo-20160313.717/bongo-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/powerline-20160504.2355/powerline-autoloads.el" "~/.emacs.d/init.el" "/Users/aries/.emacs.d/init.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/better-defaults-20160127.2318/better-defaults-autoloads.el"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 206
+  "/Users/aries/Library/Preferences/Aquamacs Emacs/Preferences.el"
+  "Preferences.el"
+  'emacs-lisp-mode
+  '(linum-mode auto-complete-mode highlight-sexp-mode rainbow-delimiters-mode rainbow-blocks-mode lispy-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . utf-8-unix)))
+
+(desktop-create-buffer 206
+  "/Users/aries/Library/Application Support/Aquamacs Emacs/scratch buffer"
+  "*scratch*"
+  'text-mode
+  '(visual-line-mode smart-spacing-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . utf-8) (truncate-lines)))
+
